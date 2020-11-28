e0040057534dd92426c9f7632ab54aa76171d7d9
IvLaptev <ivlaptev13@ya.ru>
2020-11-28 20:46:53 +0300

    Changed server connection

--- src/panels/PlayVideo.tsx
+++ src/panels/PlayVideo.tsx
@@ -5,7 +5,10 @@ const PlayVideo = ({id, link, go}) => {
     return (
         <Panel id={id}>
             <PanelHeader left={<PanelHeaderButton onClick={() => go('tournament')}><Icon24Back/></PanelHeaderButton>}>Стрим</PanelHeader>
-            <iframe src={link} width="100%" height="100%" allowFullScreen={true}></iframe>
+            <iframe src={"https://player.twitch.tv/?channel="+link.split('/').pop()+"&parent="+window.location.hostname} height="360"
+   width="640"
+   frameBorder="0"
+   scrolling="no"></iframe>
         </Panel>
     )
 }
