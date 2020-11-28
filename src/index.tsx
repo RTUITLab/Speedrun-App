7cd323171d3eac220a1badceb4b54434d800bf1c
Maksim Makushchenko <maksalmak@gmail.com>
2020-11-28 19:25:22 +0300

    Use theme in index

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import bridge from '@vkontakte/vk-bridge';
+import bridge, { UpdateConfigData } from '@vkontakte/vk-bridge';
 
 import { OpenAPI } from './api/core/OpenAPI';
 
@@ -13,7 +13,15 @@ OpenAPI.BASE = process.env.REACT_APP_BACKEND_URL || 'https://localhost:5001';
 bridge.send('VKWebAppInit');
  
 // Subscribes to event, sended by client
-bridge.subscribe(e => console.log(e));
+bridge.subscribe(e =>{
+  console.log(e);
+  if (e.detail.type === 'VKWebAppUpdateConfig') {
+    const schemeAttribute = document.createAttribute('scheme');
+    const configData = e.detail.data as UpdateConfigData;
+    schemeAttribute.value = configData.scheme ? configData.scheme : 'client_light';
+    document.body.attributes.setNamedItem(schemeAttribute);
+  } 
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
