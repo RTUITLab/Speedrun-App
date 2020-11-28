e450ba7c6c457e40d02dd29ee67e9b46166c7935
Mikhail <wowivan2011@yandex.ru>
2020-11-29 00:57:21 +0300

    Rode the table across the table

--- src/panels/GameTab/TopPage.tsx
+++ src/panels/GameTab/TopPage.tsx
@@ -2,12 +2,57 @@ import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
 import {LeaderBoardsService, Leaderboard, GamesService, Category} from '../../api';
-import Table from 'react-bootstrap/Table'
+import BTable from 'react-bootstrap/Table'
+
+
+import {useTable} from 'react-table'
+
+function Table({columns, data}) {
+    // Use the state and functions returned from useTable to build your UI
+    const {getTableProps, headerGroups, rows, prepareRow} = useTable({
+        columns,
+        data,
+    })
+
+    // Render the UI for your table
+    return (
+        <BTable striped bordered hover size="sm" variant="dark" {...getTableProps()}>
+            <thead>
+            {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => (
+                        <th {...column.getHeaderProps()}>
+                            {column.render('Header')}
+                        </th>
+                    ))}
+                </tr>
+            ))}
+            </thead>
+            <tbody>
+            {rows.map((row, i) => {
+                prepareRow(row)
+                return (
+                    <tr {...row.getRowProps()}>
+                        {row.cells.map(cell => {
+                            return (
+                                <td {...cell.getCellProps()}>
+                                    {cell.render('Cell')}
+                                </td>
+                            )
+                        })}
+                    </tr>
+                )
+            })}
+            </tbody>
+        </BTable>
+    )
+}
 
 const TopPage = (props) => {
 
     const [leadboard, setLeadboard] = useState<Leaderboard | null>(null)
     const [categories, setCategories] = useState<Array<Category> | null>(null)
+    const [testA, setTestA] = useState<Array<leadbordTest>|null>([])
     useEffect(() => {
 
         async function fetchCategory() {
@@ -28,37 +73,63 @@ const TopPage = (props) => {
             if (leadboard === null && categories !== null) {
                 const data = await LeaderBoardsService.getLeaderboard(props.id, categories[0].id!);
                 setLeadboard(() => data);
+                testData(data);
             }
         }
     })
 
+    const testData = (lb) => {
+        const data = lb?.runs?.map(r => ({
+            place: r.place+'',
+            name: (r.run?.players ? r.run?.players[0].name : "") || "",
+            time: (r.run?.times?.primary) || "",
+            platform: leadboard?.platform+'',
+            old: r
+        })) as leadbordTest[];
+        setTestA(data);
+        console.log(data);
+    }
+
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'Место',
+                accessor: 'place'
+            },
+            {
+                Header: 'Игрок',
+                accessor: 'name'
+            },
+            {
+                Header: 'Время',
+                accessor: 'time'
+            },
+            {
+                Header: 'Платформа',
+                accessor: 'platform'
+            }
+        ],
+        []
+    )
+
 
     return (
-        <Table striped bordered hover size="sm" variant="dark">
-            <thead>
-            <tr>
-                <th>Место</th>
-                <th>Игрок</th>
-                <th>Время</th>
-                <th>Платформа</th>
-            </tr>
-            </thead>
-            <tbody>
-            {leadboard !== null && leadboard.runs && leadboard.runs.map(r => {
-                <tr>
-                    <td>{r.place}</td>
-                    <td>{r.run && r.run.players && r.run.players[0].name}</td>
-                    <td>{r.run && r.run.times && r.run.times}</td>
-                    <td>{leadboard.platform}</td>
-                </tr>
-            })}
-            </tbody>
-        </Table>
+        <div>
+            <Table columns={columns} data={testA}/>
+        </div>
     )
 }
 
 TopPage.prototype = {
     id: PropTypes.string.isRequired
+}
+
+
+export type leadbordTest = {
+    place: string,
+    name: string,
+    time: string,
+    platform: string
 }
 
 export default TopPage;
