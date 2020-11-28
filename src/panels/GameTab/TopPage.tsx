af4a7a4abc7255c62bf8738c84742472bfedba8a
Mikhail <wowivan2011@yandex.ru>
2020-11-29 02:17:02 +0300

    delete bootstrap

--- src/panels/GameTab/TopPage.tsx
+++ src/panels/GameTab/TopPage.tsx
@@ -2,49 +2,48 @@ import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
 import {LeaderBoardsService, Leaderboard, GamesService, Category} from '../../api';
-import BTable from 'react-bootstrap/Table'
 
 
 import {useTable} from 'react-table'
 
 function Table({columns, data}) {
     // Use the state and functions returned from useTable to build your UI
-    const {getTableProps, headerGroups, rows, prepareRow} = useTable({
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({
         columns,
         data,
     })
 
     // Render the UI for your table
     return (
-        <BTable striped bordered hover size="sm" variant="dark" {...getTableProps()}>
+        <table {...getTableProps()}>
             <thead>
             {headerGroups.map(headerGroup => (
                 <tr {...headerGroup.getHeaderGroupProps()}>
                     {headerGroup.headers.map(column => (
-                        <th {...column.getHeaderProps()}>
-                            {column.render('Header')}
-                        </th>
+                        <th {...column.getHeaderProps()}>{column.render('Header')}</th>
                     ))}
                 </tr>
             ))}
             </thead>
-            <tbody>
+            <tbody {...getTableBodyProps()}>
             {rows.map((row, i) => {
                 prepareRow(row)
                 return (
                     <tr {...row.getRowProps()}>
                         {row.cells.map(cell => {
-                            return (
-                                <td {...cell.getCellProps()}>
-                                    {cell.render('Cell')}
-                                </td>
-                            )
+                            return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
                         })}
                     </tr>
                 )
             })}
             </tbody>
-        </BTable>
+        </table>
     )
 }
 
